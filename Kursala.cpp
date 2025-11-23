db98b2727256c3f9c1f2424c2f4c5028f9073981
RuslanDragan <draganruslan183@gmail.com>
2025-11-23 17:42:45 +0300

    Update Kursala.cpp

--- Kursala.cpp
+++ Kursala.cpp
@@ -28,7 +28,7 @@ public:
 Graph::Graph(int V) {
     this->V = V;
     parent = new int[V];
-    // Ініціалізація: кожен елемент є батьком сам собі
+
     for (int i = 0; i < V; i++)
         parent[i] = i;
 
@@ -37,16 +37,16 @@ Graph::Graph(int V) {
 }
 
 void Graph::AddWeightedEdge(int u, int v, int w) {
-    // Зверніть увагу: ми передаємо вершини як 1-8, але в масив пишемо як 0-7
+
     G.push_back(make_pair(w, edge(u - 1, v - 1)));
 }
 
 int Graph::find_set(int i) {
-    // Якщо i є батьком самого себе
+
     if (i == parent[i])
         return i;
     else
-        // Рекурсивний пошук кореня множини
+
         return find_set(parent[i]);
 }
 
@@ -56,17 +56,17 @@ void Graph::union_set(int u, int v) {
 
 void Graph::kruskal() {
     int i, uRep, vRep;
-    // Сортуємо всі ребра за зростанням ваги
+
     sort(G.begin(), G.end());
 
     for (i = 0; i < G.size(); i++) {
         uRep = find_set(G[i].second.first);
         vRep = find_set(G[i].second.second);
 
-        // Якщо вершини в різних множинах (немає циклу)
+
         if (uRep != vRep) {
-            T.push_back(G[i]); // додаємо ребро в дерево
-            union_set(uRep, vRep); // об'єднуємо множини
+            T.push_back(G[i]);
+            union_set(uRep, vRep); 
         }
     }
 }
@@ -75,7 +75,7 @@ void Graph::print() {
     int total_weight = 0;
     cout << "Edge :" << " Weight" << endl;
     for (int i = 0; i < T.size(); i++) {
-        // Виводимо +1, щоб номери співпадали зі схемою (1-8)
+
         cout << T[i].second.first + 1 << " - " << T[i].second.second + 1 << " : "
              << T[i].first;
         cout << endl;
@@ -85,11 +85,10 @@ void Graph::print() {
 }
 
 int main() {
-    // Створюємо граф на 8 вершин
+
     Graph g(8);
 
-    // Додаємо всі ребра
-    // (u, v, weight)
+
     g.AddWeightedEdge(1, 3, 1);
     g.AddWeightedEdge(1, 4, 4);
     g.AddWeightedEdge(1, 2, 4);
@@ -109,11 +108,12 @@ int main() {
 
     g.AddWeightedEdge(7, 8, 5);
 
-    // Запускаємо алгоритм
+
     g.kruskal();
 
-    // Виводимо результат
+
     g.print();
 
     return 0;
+
 }
