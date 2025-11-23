924cc32dcc9271a7fa570a61145343b9c71a176b
RuslanDragan <draganruslan183@gmail.com>
2025-11-23 17:41:56 +0300

    Update Prima.cpp

--- Prima.cpp
+++ Prima.cpp
@@ -2,14 +2,11 @@
 #include <cstring>
 using namespace std;
 
-// Âèçíà÷àºìî íåñê³í÷åíí³ñòü ÿê äóæå âåëèêå ÷èñëî
 #define INF 9999999
 
-// Ê³ëüê³ñòü âåðøèí ó ãðàô³
 #define V 8
 
-// Ìàòðèöÿ ñóì³æíîñò³ äëÿ Âàð³àíòó
-// 0 - â³äñòàíü äî ñåáå, INF - â³äñóòí³ñòü ðåáðà, ÷èñëà - âàãà ðåáðà
+
 int G[V][V] = {
   // 1    2    3    4    5    6    7    8
   {0,   4,   1,   4, INF, INF, INF, INF}, // 1
@@ -23,42 +20,41 @@ int G[V][V] = {
 };
 
 int main() {
-  int no_edge; // ë³÷èëüíèê ðåáåð
+  int no_edge; 
 
-  // Ìàñèâ äëÿ â³äñòåæåííÿ îáðàíèõ âåðøèí
-  // selected áóäå true, ÿêùî âåðøèíà âæå âêëþ÷åíà â ÌÊÄ
+
   bool selected[V];
 
-  // Ñïî÷àòêó âñ³ false
+
   memset(selected, false, sizeof(selected));
 
-  // Ïî÷èíàºìî ç 0 ðåáåð
+
   no_edge = 0;
 
   selected[0] = true;
 
-  int x; // íîìåð ðÿäêà (âåðøèíà ç)
-  int y; // íîìåð ñòîâïöÿ (âåðøèíà â)
+  int x; 
+  int y; 
 
   cout << "Edge" << " : " << "Weight";
   cout << endl;
 
-  int total_weight = 0; // Çì³ííà äëÿ ï³äðàõóíêó çàãàëüíî¿ âàãè
+  int total_weight = 0; 
 
-  // Öèêë ïðàöþº, ïîêè ê³ëüê³ñòü ðåáåð ìåíøà çà V - 1
+
   while (no_edge < V - 1) {
 
     int min = INF;
     x = 0;
     y = 0;
 
-    // Äëÿ êîæíî¿ âåðøèíè, ÿêà âæå â äåðåâ³ (selected[i] == true)
+
     for (int i = 0; i < V; i++) {
       if (selected[i]) {
-        // Øóêàºìî ñóñ³äíþ âåðøèíó, ÿêî¿ ùå íåìàº â äåðåâ³ (selected[j] == false)
+
         for (int j = 0; j < V; j++) {
-          if (!selected[j] && G[i][j]) { // ÿêùî âåðøèíà íå îáðàíà ³ º ðåáðî
-            // ßêùî öå ðåáðî ìåíøå çà ïîòî÷íèé ì³í³ìóì
+          if (!selected[j] && G[i][j]) { 
+
             if (min > G[i][j]) {
               min = G[i][j];
               x = i;
@@ -72,8 +68,8 @@ int main() {
     cout << x + 1 << " - " << y + 1 << " : " << G[x][y];
     cout << endl;
 
-    total_weight += G[x][y]; // Äîäàºìî âàãó äî ñóìè
-    selected[y] = true;      // Ïîçíà÷àºìî âåðøèíó ÿê äîäàíó
+    total_weight += G[x][y]; 
+    selected[y] = true;     
     no_edge++;
   }
 
